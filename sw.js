54a723beb8df439f0e5f4df5b4a943b1a3b94638
wzw1010 <19012901190@126.com>
2026-06-23 00:52:03 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+// Fund Holder · Service Worker
+const CACHE_NAME = 'fund-holder-v1';
+const urlsToCache = [
+  '/my-funds/',
+  '/my-funds/index.html',
+  '/my-funds/manifest.json',
+  '/my-funds/icon-1024.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
